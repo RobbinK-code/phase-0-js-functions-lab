27289bc0b8ea2e93d30dca9f98e2cb5b87386d4a
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-03-30 17:36:57 +0300

    Added new file and functions.

---
+++ main.js
@@ -0,0 +1,29 @@
+function calculateTax(amount){
+    return amount * 0.10
+}
+amount = Number(prompt("Enter the amount: "))
+console.log(calculateTax(amount))
+
+function convertToUpperCase(text){
+    return text.toUpperCase()
+}
+console.log(convertToUpperCase(inputText))
+
+function findMaximum(num1, num2){
+    return Math.max(num1, num2)
+}
+console.log(findMaximum(num1, num2))
+
+function isPalindrome(word){
+    if word===palindrome {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isPalindrome(word))
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    return originalPrice - (originalPrice * (discountPercentage / 100))
+}
+console.log(calculateDiscountedPrice(originalPrice, discountPercentage))
