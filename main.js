cc83328317478c764d9ea2e5324124945bd723ee
RobbinK-code <robbin.kimani@student.moringaschool.com>
2026-03-30 18:13:13 +0300

    Updated code for functions.

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-function calculateTax(amount){
-    return amount * 0.10
+function calculateTax(amount) {
+  return amount * 0.10;
 }
-amount = Number(prompt("Enter the amount: "))
-console.log(calculateTax(amount))
 
-function convertToUpperCase(text){
-    return text.toUpperCase()
+function convertToUpperCase(text) {
+  return text.toUpperCase();
 }
-console.log(convertToUpperCase(inputText))
 
-function findMaximum(num1, num2){
-    return Math.max(num1, num2)
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
 }
-console.log(findMaximum(num1, num2))
 
-function isPalindrome(word){
-    if word===palindrome {
-        return true
-    } else {
-        return false
-    }
+function isPalindrome(word) {
+  const cleanedWord = word.toLowerCase();
+  const reversedWord = cleanedWord.split('').reverse().join('');
+  return cleanedWord === reversedWord;
 }
-console.log(isPalindrome(word))
 
-function calculateDiscountedPrice(originalPrice, discountPercentage){
-    return originalPrice - (originalPrice * (discountPercentage / 100))
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * (discountPercentage / 100));
 }
-console.log(calculateDiscountedPrice(originalPrice, discountPercentage))
